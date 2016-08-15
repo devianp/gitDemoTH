34406cb80c8ec65923baf1b4bf198de96e6025b3
devianp <devianp@gmail.com>
2016-08-15 21:17:28 +0100

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Devian Patel on 15/08/2016.
+//  Copyright © 2016 Devian Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
